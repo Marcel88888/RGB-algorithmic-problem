3c0ea5ef97d6ae9fe7215c8cd69e1b06300f7157
Marcel88888 <marcel.kawski1@gmail.com>
2021-01-12 20:49:45 +0100

    Accessory functions for naive sorting implemented.

--- src/algorithm_lib/naiveSorting.cpp
+++ src/algorithm_lib/naiveSorting.cpp
@@ -2,278 +2,179 @@
 
 using namespace std;
 
-//Solution NaiveSorting::sort(const vector<RgbElement> &elements, int maxRgbGroupsAmount) {
-//    vector<RgbElement> elementsCopy(elements);
-//    int size = sizeof(RGB_ELEMENTS)/sizeof(RGB_ELEMENTS[0]);; // 3
-//    for (int i = 0; i < maxRgbGroupsAmount * size; ++i) {
-//        if (elementsCopy[i] == RGB_ELEMENTS[i % size]) {
-//            continue;
-//        }
-//        else {
-//            for (int j = i + int(size - (elementsCopy.size() % size));
-//                j < elementsCopy.size() - size; j += size) {
-//                cout << j << endl;
-//                if (elementsCopy[j] == RGB_ELEMENTS[i % size]) {
-//                    int moves = 0;
-//                    if (elementsCopy.size() % size == 0) { //  [x, ?, ?]
-//                        moveTripleBack(elementsCopy, j);
-//                        moves = int((elementsCopy.size() - 3) / size);
-//                    }
-//                    else if (elementsCopy.size() % size == 1) {  // [?, ?, x]
-//                        moveTripleBack(elementsCopy, j-2);
-//                        moves = int((elementsCopy.size() - 1) / size);
-//                    }
-//                    else { // [?, x, ?]
-//                        moveTripleBack(elementsCopy, j-1);
-//                        moves = int((elementsCopy.size() - 2) / size);
-//                    }
-//                    for (int k = 0; k < moves; ++k) {
-//                        moveTripleBack(elementsCopy, i);
-//                    }
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//    return Solution(move(elementsCopy));
-//}
+
+int findExpValTriplePosition(int prevPosition, vector<RgbElement> elementsCopy, RgbElement expectedValue,
+                              int displacement) {
+    for(int j = prevPosition+1; j < elementsCopy.size(); ++j) {
+        if (elementsCopy[j] == expectedValue)
+            return j - displacement;
+    }
+    return -1;
+}
+
+vector<RgbElement> moveExpValToTheBeg(int requiredMoves, vector<RgbElement> elementsCopy, int moveIndex) {
+    int k = 0;
+    while (k < requiredMoves) {
+        moveTripleBack(elementsCopy, moveIndex);
+        ++k;
+    }
+    return elementsCopy;
+}
+
+
+/*
+ * Algorithm: First we have a problem where there must be a letter x under the index [0] (usually we start with x = R).
+ * We try to search for x in 3k places, if we find it then we just move the first three to the end so it starts with x.
+ * If there is not the desired value in those places, then we look for the index of the first x encountered (iterating
+ * from bottom to top) and try put it to the beginning. Depending on the number of balls, there are 3 cases:
+ * - size (input) == 3n ⇒ take the three of the x at the beginning [x,?,?] and put it at the end and keep moving all the
+ * first three at the end until x stops at the beginning.
+ * - size (input) == 3n + 1 ⇒ take the triple of x at the end [?,?,x] and move it to the end. Then again, taking all
+ * three of them out of the beginning, x at some point will stand in the desired place.
+ * size (input) == 3n + 2 ⇒ you have to take the three of x so that x stands in 2nd place [?,x,?] And take the first
+ * three.
+*/
 
 Solution NaiveSorting::sort(const vector<RgbElement> &elements, int maxRgbGroupsAmount) {
     vector<RgbElement> elementsCopy(elements);
-    int colors_number = sizeof(RGB_ELEMENTS)/sizeof(RGB_ELEMENTS[0]); // 3
-    for(int i = 0; i < maxRgbGroupsAmount * colors_number; ++i) {
+    int colors_number = sizeof(RGB_ELEMENTS) / sizeof(RGB_ELEMENTS[0]); // 3
+    for (int i = 0; i < maxRgbGroupsAmount * colors_number; ++i) {
         RgbElement expected_value = RGB_ELEMENTS[i % colors_number];
         if (elementsCopy[i] == expected_value) {
             continue;
         }
-        int position = -1;
-        for(int j = i+1; j < elementsCopy.size(); ++j) {
-            if (elementsCopy[j] == expected_value) {
-                position = j;
-                break;
-            }
-        }
+        int position = findExpValTriplePosition(i, elementsCopy, expected_value, 0);
         if (expected_value == RgbElement::R) {
             int moves;
-            int k = 0;
             if (position % colors_number == 0) {  // on position 3k
                 moves = (position - i) / colors_number;
-                while (k < moves) {
-                    moveTripleBack(elementsCopy, i);
-                    ++k;
-                }
+                elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
             }
             else if (position % colors_number == 1 || position % colors_number == 2) {
                 if (elementsCopy.size() % colors_number == 0) {
                     int move_position = position;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 0);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 3 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
                 else if (elementsCopy.size() % colors_number == 1) {
                     int move_position = position - 2;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l - 2;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 2);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 1 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
                 else if (elementsCopy.size() % colors_number == 2) {
                     int move_position = position - 1;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l - 1;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 1);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 2 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
             }
         }
         else if (expected_value == RgbElement::G) {
             int moves;
-            int k = 0;
-            if (position % colors_number == 1) {  // on position 3k + 1
+            if (position % colors_number == 1) {  // on position n 3k + 1
                 moves = (position - i) / colors_number;
-                while (k < moves) {
-                    moveTripleBack(elementsCopy, i);
-                    ++k;
-                }
+                elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
             }
             else if (position % colors_number == 2 || position % colors_number == 0) {
                 if (elementsCopy.size() % colors_number == 0) {
                     int move_position = position - 1;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l - 1;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 1);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 2 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
                 else if (elementsCopy.size() % colors_number == 1) {
                     int move_position = position;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 0);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 3 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
                 else if (elementsCopy.size() % colors_number == 2) {
                     int move_position = position - 2;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l - 2;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 2);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 1 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
             }
         }
         else if (expected_value == RgbElement::B) {
             int moves;
-            int k = 0;
             if (position % colors_number == 2) {  // on position 3k + 2
                 moves = (position - i) / colors_number;
-                while (k < moves) {
-                    moveTripleBack(elementsCopy, i);
-                    ++k;
-                }
+                elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
             }
             else if (position % colors_number == 0 || position % colors_number == 1) {
                 if (elementsCopy.size() % colors_number == 0) {
                     int move_position = position - 2;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l - 2;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 2);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 1 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
                 else if (elementsCopy.size() % colors_number == 1) {
                     int move_position = position - 1;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l - 1;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 1);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 2 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
                 else if (elementsCopy.size() % colors_number == 2) {
                     int move_position = position;
-                    if (move_position < i) { // because sometimes we can move already sorted balls
-                        for(int l = position+1; l < elementsCopy.size(); ++l) {
-                            if(elementsCopy[l] == expected_value) {
-                                move_position = l;
-                                break;
-                            }
-                        }
+                    if (move_position < i) { // because sometimes we may move already sorted balls and we need to find
+                        // expected value further
+                        move_position = findExpValTriplePosition(position, elementsCopy, expected_value, 0);
                     }
                     moveTripleBack(elementsCopy, move_position);
                     moves = (int(elementsCopy.size()) - 3 - i) / colors_number;
-                    while (k < moves) {
-                        moveTripleBack(elementsCopy, i);
-                        ++k;
-                    }
+                    elementsCopy = moveExpValToTheBeg(moves, elementsCopy, i);
                 }
             }
         }
     }
 
     int well = 0;
-//    for(int i = 0; i < elementsCopy.size(); i += colors_number) {
-//        if(elementsCopy[i] == RgbElement::R && elementsCopy[i+1] == RgbElement::G &&
-//           elementsCopy[i+2] == RgbElement::B) {
-//            well++;
-//        }
-//    }
-//
-//    int z = 0;
-//    while(z < well * colors_number) {
-//        if(elementsCopy[z] == RgbElement::R && elementsCopy[z+1] == RgbElement::G &&
-//        elementsCopy[z+2] == RgbElement::B) {
-//            z += colors_number;
-//        }
-//        else { moveTripleBack(elementsCopy, z); }
-//    }
-
-    well = 0;
-    for(int i = 0; i < elementsCopy.size(); i += colors_number) {
-        if(elementsCopy[i] == RgbElement::R && elementsCopy[i+1] == RgbElement::G &&
-           elementsCopy[i+2] == RgbElement::B) {
+    for (int i = 0; i < elementsCopy.size(); i += colors_number) {
+        if (elementsCopy[i] == RgbElement::R && elementsCopy[i + 1] == RgbElement::G &&
+            elementsCopy[i + 2] == RgbElement::B) {
             well++;
-        }
-        else {
+        } else {
             break;
         }
     }
     cout << "Well: " << well << std::endl;
     return Solution(elementsCopy);
 }
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main() {
 
 
 
-    const int kElementsNumber = 102;
+    const int kElementsNumber = 1000;
     std::vector<RgbElement> elements(kElementsNumber);
 
     std::generate(elements.begin(), elements.end(),[] {
@@ -50,23 +50,65 @@ int main() {
     std::cout << s << std::endl;
 
     bool correct = areElementsArrangedCorrectly(s.arrangedElements, max);
-    cout << "Correctly: " << correct << std::endl;
+    cout << "Correctly: " << correct << std::endl << std::endl;
 
-//    const int kElementsNumber = 10;
-//    std::vector<RgbElement> elements(kElementsNumber);
-//    std::generate(elements.begin(), elements.end(),[] {
-//        return RGB_ELEMENTS[randomUniformInteger(0, 2)];
-//    });
-//
-//    std::cout << elements << std::endl;
-//    moveTripleBack(elements, kElementsNumber-1);
-//    std::cout << elements << std::endl;
-//    moveTripleBack(elements, kElementsNumber-2);
-//    std::cout << elements << std::endl;
-//    moveTripleBack(elements, kElementsNumber-3);
-//    std::cout << elements << std::endl << std::endl;
-//
-//    moveTripleBack(elements, kElementsNumber-4);
-//    std::cout << elements << std::endl;
+
+
+
+    const int kElementsNumber2 = 1001;
+    std::vector<RgbElement> elements2(kElementsNumber2);
+
+    std::generate(elements2.begin(), elements2.end(),[] {
+        return RGB_ELEMENTS[randomUniformInteger(0, 2)];
+    });
+
+    std::cout << elements2 << std::endl;
+    int r2 = 0, g2 = 0, b2 = 0;
+    for (auto & element : elements2) {
+        if(element == RgbElement::R) r2++;
+        else if(element == RgbElement::G) g2++;
+        else b2++;
+    }
+    std::cout << "R: " << r2 << std::endl << "G: " << g2 << std::endl << "B: " << b2 << std::endl;
+
+    int max2 = maxRgbGroupsAmount(elements2);
+
+    cout << "max: " << max2 << std::endl;
+
+    Solution s2 = NaiveSorting::sort(elements2, max2);
+    std::cout << s2 << std::endl;
+
+    bool correct2 = areElementsArrangedCorrectly(s2.arrangedElements, max2);
+    cout << "Correctly: " << correct2 << std::endl << std::endl;
+
+
+
+
+    const int kElementsNumber3 = 1002;
+    std::vector<RgbElement> elements3(kElementsNumber3);
+
+    std::generate(elements3.begin(), elements3.end(),[] {
+        return RGB_ELEMENTS[randomUniformInteger(0, 2)];
+    });
+
+    std::cout << elements3 << std::endl;
+    int r3 = 0, g3 = 0, b3 = 0;
+    for (auto & element : elements3) {
+        if(element == RgbElement::R) r3++;
+        else if(element == RgbElement::G) g3++;
+        else b3++;
+    }
+    std::cout << "R: " << r3 << std::endl << "G: " << g3 << std::endl << "B: " << b3 << std::endl;
+
+    int max3 = maxRgbGroupsAmount(elements3);
+
+    cout << "max: " << max3 << std::endl;
+
+    Solution s3 = NaiveSorting::sort(elements3, max3);
+    std::cout << s3 << std::endl;
+
+    bool correct3 = areElementsArrangedCorrectly(s3.arrangedElements, max3);
+    cout << "Correctly: " << correct3 << std::endl << std::endl;
+
     return 0;
 }
