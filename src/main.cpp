bb083e3e720c6b4074df2ee3e200c0f4358e8776
Marcel88888 <marcel.kawski1@gmail.com>
2021-01-21 01:38:24 +0100

    Main loop implemented.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,31 @@
 #include "algorithm_lib/NaiveSorting.h"
 #include "algorithm_lib/InitialTripleSearch.h"
 #include <vector>
+#include <bits/stdc++.h>
 
+
+void printHelp()
+{
+    cout << "Help:" << endl << endl;
+    cout << "exit - exit program" << endl;
+    cout << "naive - NaiveSorting" << endl;
+    cout << "initial - InitialTripleSearch" << endl;
+    cout << "advanced - AdvancedSearch" << endl;
+    cout << "breadth - BreadthSearch" << endl;
+    cout << endl;
+}
+
+vector<string> splitWords(const string& str)
+{
+    vector<string> splitWords;
+    istringstream ss(str);
+    string word;
+    while (ss >> word)
+    {
+        splitWords.push_back(word);
+    }
+    return splitWords;
+}
 
 int main() {
 //    const int kElementsNumber = 10;
@@ -87,35 +111,102 @@ int main() {
 
 
 
-    const int kElementsNumber3 = 1002;
-    std::vector<RgbElement> elements3(kElementsNumber3);
+//    const int kElementsNumber3 = 1002;
+//    std::vector<RgbElement> elements3(kElementsNumber3);
+//
+//    std::generate(elements3.begin(), elements3.end(),[] {
+//        return kRgbElements[randomUniformInteger(0, 2)];
+//    });
+//
+//
+//    std::cout << elements3.size() << std::endl;
+//    int r3 = 0, g3 = 0, b3 = 0;
+//    for (auto & element : elements3) {
+//        if(element == RgbElement::R) r3++;
+//        else if(element == RgbElement::G) g3++;
+//        else b3++;
+//    }
+//    std::cout << "R: " << r3 << std::endl << "G: " << g3 << std::endl << "B: " << b3 << std::endl;
+//
+//    int max3 = maxRgbGroupsAmount(elements3);
+//
+//    cout << "max: " << max3 << std::endl;
+//
+////    Solution s3 = NaiveSorting::sort(elements3, max3);
+//    Solution s3 = InitialTripleSearch::sort(elements3, 100);
+//    std::cout << s3.arrangedElements << std::endl;
+//    std::cout << s3.indexesOfMovedGroups << std::endl;
+////    std::cout << "Moves: " << s3.indexesOfMovedGroups << std::endl;
+//
+//    bool correct3 = areElementsArrangedCorrectly(s3.arrangedElements, max3);
+//    cout << "Correctly: " << correct3 << std::endl << std::endl;
 
-    std::generate(elements3.begin(), elements3.end(),[] {
-        return kRgbElements[randomUniformInteger(0, 2)];
-    });
+    bool running = true;
+    string command;
 
+    while (running)
+    {
+        vector<string> command_words;
+        cout << "rgbRobot$ ";
+        getline(cin, command);
+        command_words = splitWords(command);
+        cout << command_words << endl;
 
-    std::cout << elements3.size() << std::endl;
-    int r3 = 0, g3 = 0, b3 = 0;
-    for (auto & element : elements3) {
-        if(element == RgbElement::R) r3++;
-        else if(element == RgbElement::G) g3++;
-        else b3++;
+        if (command_words[0] == "exit" && command_words.size() == 1)
+            running = false;
+        else if (command_words[0] == "naive") {
+            if (command_words[1] == "-m1") {
+                if (command_words[2] == "<<" && command_words[4] == ">>" && command_words.size() == 6) {
+
+                }
+                else
+                    cout << "Invalid command." << endl;
+            } else if (command_words[1] == "-m2") {
+
+            } else if (command_words[1] == "-m3") {
+
+            }
+            else
+                cout << "Invalid command." << endl;
+        }
+        else if (command_words[0] == "initial") {
+            if (command_words[1] == "-m1") {
+
+            } else if (command_words[1] == "-m2") {
+
+            } else if (command_words[1] == "-m3") {
+
+            }
+            else
+                cout << "Invalid command." << endl;
+        }
+        else if (command_words[0] == "advanced") {
+            if (command_words[1] == "-m1") {
+
+            } else if (command_words[1] == "-m2") {
+
+            } else if (command_words[1] == "-m3") {
+
+            }
+            else
+                cout << "Invalid command." << endl;
+        }
+        else if (command_words[0] == "breadth") {
+            if (command_words[1] == "-m1") {
+
+            } else if (command_words[1] == "-m2") {
+
+            } else if (command_words[1] == "-m3") {
+
+            }
+            else
+                cout << "Invalid command." << endl;
+        }
+        else if (command_words[0] == "help" && command_words.size() == 1)
+            printHelp();
+        else
+            cout << "Command not found." << endl;
     }
-    std::cout << "R: " << r3 << std::endl << "G: " << g3 << std::endl << "B: " << b3 << std::endl;
-
-    int max3 = maxRgbGroupsAmount(elements3);
-
-    cout << "max: " << max3 << std::endl;
-
-//    Solution s3 = NaiveSorting::sort(elements3, max3);
-    Solution s3 = InitialTripleSearch::sort(elements3, 100);
-    std::cout << s3.arrangedElements << std::endl;
-    std::cout << s3.indexesOfMovedGroups << std::endl;
-//    std::cout << "Moves: " << s3.indexesOfMovedGroups << std::endl;
-
-    bool correct3 = areElementsArrangedCorrectly(s3.arrangedElements, max3);
-    cout << "Correctly: " << correct3 << std::endl << std::endl;
 
     return 0;
 }
